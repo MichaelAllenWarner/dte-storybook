036f918142fb93aee362415a5a4c483c5ccca6a0
Michael Warner <michaelwarner@Michaels-MacBook-Pro.local>
2021-12-31 14:40:59 -0500

    broken dte-alpha build, with some comments

--- .storybook/twing-environment.js
+++ .storybook/twing-environment.js
@@ -1,8 +1,21 @@
-const {
+/*
+  `import` (instead of `require`) needed for drupal-twig-extensions 1.0.0-alpha.6,
+  but `npm run storybook` results in:
+
+  `SyntaxError: Cannot use import statement outside a module`.
+
+  I've also tried dynamic `import()` calls, including wrapping everything
+  in an async IIFE, but that errors out too.
+
+  As far as I can tell, drupal-twig-extensions 1.0.0-alpha.6 is incompatible
+  with this Storybook / Webpack 5 setup.
+*/
+
+import {
   TwingEnvironment,
   TwingLoaderRelativeFilesystem,
-} = require('twing');
-const { addDrupalExtensions } = require('drupal-twig-extensions/twing');
+} from 'twing';
+import { addDrupalExtensions } from 'drupal-twig-extensions/twing';
 
 const twing = new TwingEnvironment(
   new TwingLoaderRelativeFilesystem(),
@@ -11,4 +24,5 @@ const twing = new TwingEnvironment(
 
 addDrupalExtensions(twing);
 
-module.exports = twing;
+// I think could be `module.exports = { default: twing }` if needed?
+export default twing;
