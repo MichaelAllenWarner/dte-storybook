08f21b03123faa0c48570d8efd7f8a18f68fd791
Michael Warner <michaelwarner@Michaels-MacBook-Pro.local>
2022-03-16 09:33:04 -0400

    with working drupal-attribute

--- components/Example/Example.stories.js
+++ components/Example/Example.stories.js
@@ -1,7 +1,12 @@
 import ExampleTwig from './Example.twig';
+import DrupalAttribute from '../../DrupalAttribute';
 
 export default { title: 'Example' };
 
 export const Example = (_, { loaded: { renderedStory } }) => renderedStory;
 
 Example.render = ExampleTwig;
+
+Example.args = {
+  attributes: new DrupalAttribute()
+};
